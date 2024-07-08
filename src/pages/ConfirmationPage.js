22d9adfd6b7b97341fff1b08deec6a37eccd640e
prius2055 <prius2055@gmail.com>
2024-07-08 12:09:42 +0100

    Added deploy branch

--- src/pages/ConfirmationPage.js
+++ src/pages/ConfirmationPage.js
@@ -16,6 +16,7 @@ const ConfirmationPage = () => {
 
   const email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$/;
 
+  
   const handleSubmission = async (e) => {
     e.preventDefault();
     if (details.userEmail.match(email)) {
