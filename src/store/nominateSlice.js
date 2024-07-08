51293af4bd8b0ae05362362c25a1c709fd3ddfaa
prius2055 <prius2055@gmail.com>
2024-07-08 17:08:46 +0100

    Update the backend api in master branch

--- src/store/nominateSlice.js
+++ src/store/nominateSlice.js
@@ -6,7 +6,7 @@ export const postNomination = createAsyncThunk(
   async (details, { rejectWithValue }) => {
     try {
       const response = await axios.post(
-        `https://pafir-awards-backend.vercel.app/api/nominations`,
+        `http://localhost:3000/api/v1/nominations`,
         details,
         {
           headers: {
@@ -30,7 +30,7 @@ export const getAllNominations = createAsyncThunk(
   'get/nominations',
   async () => {
     const response = await axios.get(
-      `https://pafir-awards-backend.vercel.app/api/nominations`,
+      `http://localhost:3000/api/v1/nominations`,
       {
         headers: {
           'content-type': 'application/json',
