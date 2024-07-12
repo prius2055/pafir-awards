0b7f272d7319682e839dab9f9154f343f4e67ba5
prius2055 <prius2055@gmail.com>
2024-07-12 14:11:19 +0100

    Remove Home.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import Header from '../components/Header';
 import Category from '../components/Category';
 import Process from '../components/Process';
 import Hero from '../components/Hero';
-import './Home.css';
 
 const Home = () => {
   return (
