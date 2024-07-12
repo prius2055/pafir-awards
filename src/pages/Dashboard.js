f0a0769701902e4da855a2ce924edd394589b876
prius2055 <prius2055@gmail.com>
2024-07-12 13:59:10 +0100

    Added print Option

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useRef } from 'react';
 import Logo from '../img/pafir-logo.png';
-
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllNominations } from '../store/nominateSlice';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import { useReactToPrint } from 'react-to-print';
 import './Dashboard.css';
 
 const Dashboard = () => {
   const { isLoading, error, nominationsArray } = useSelector(
     (state) => state.nominations
   );
+
+  const componentRef = useRef();
   const dispatch = useDispatch();
   // const navigate = useNavigate();
 
@@ -28,6 +30,10 @@ const Dashboard = () => {
     (nomination) => nomination.field.toLowerCase() === 'real estate'
   );
 
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
   if (isLoading) {
     return <p>Loading...</p>;
   }
@@ -44,7 +50,7 @@ const Dashboard = () => {
           <img src={Logo} alt="Pafir logo" className="dashboard-logo" />
         </Link>
       </div>
-      <div className="dashboard">
+      <div className="dashboard" ref={componentRef}>
         <h2>Nomination results</h2>
         <div>
           {nominationsArray && nominationsArray?.nominations?.length > 0 && (
@@ -58,6 +64,7 @@ const Dashboard = () => {
                   <th>Name of nominee</th>
                   <th>Category</th>
                   <th>Nominator's Email</th>
+                  <th>Nomination date/time</th>
                 </tr>
               </thead>
               <tbody>
@@ -67,6 +74,11 @@ const Dashboard = () => {
                     <td>{finance.nomineeName}</td>
                     <td>{finance.category}</td>
                     <td>{finance.userEmail}</td>
+                    <td>
+                      {moment(finance.createdAt).format(
+                        'YYYY-MM-DD / HH:mm:ss'
+                      )}
+                    </td>
                   </tr>
                 ))}
               </tbody>
@@ -84,6 +96,7 @@ const Dashboard = () => {
                   <th>Name of Nominee</th>
                   <th>Category</th>
                   <th>Nominator's Email</th>
+                  <th>Nomination date/time</th>
                 </tr>
               </thead>
               <tbody>
@@ -93,6 +106,11 @@ const Dashboard = () => {
                     <td>{finance.nomineeName}</td>
                     <td>{finance.category}</td>
                     <td>{finance.userEmail}</td>
+                    <td>
+                      {moment(finance.createdAt).format(
+                        'YYYY-MM-DD / HH:mm:ss'
+                      )}
+                    </td>
                   </tr>
                 ))}
               </tbody>
@@ -110,6 +128,7 @@ const Dashboard = () => {
                   <th>Name of Nominee</th>
                   <th>Category</th>
                   <th>Nominator's Email</th>
+                  <th>Nomination date/time</th>
                 </tr>
               </thead>
               <tbody>
@@ -119,10 +138,21 @@ const Dashboard = () => {
                     <td>{finance.nomineeName}</td>
                     <td>{finance.category}</td>
                     <td>{finance.userEmail}</td>
+                    <td>
+                      {moment(finance.createdAt).format(
+                        'YYYY-MM-DD / HH:mm:ss'
+                      )}
+                    </td>
                   </tr>
                 ))}
               </tbody>
             </table>
+          )}
+
+          {nominationsArray && nominationsArray?.nominations?.length > 0 && (
+            <button onClick={handlePrint} className="print">
+              Print
+            </button>
           )}
         </div>
 
